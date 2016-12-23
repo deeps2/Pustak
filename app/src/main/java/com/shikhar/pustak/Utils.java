a2782d2a6f2a258d3293af1ab9fc595765d19730
SHIKHAR DEEP <deepshikhar24@yahoo.com>
2016-12-23 19:41:26 +0530

    fixed the text of an error message

--- app/src/main/java/com/shikhar/pustak/Utils.java
+++ app/src/main/java/com/shikhar/pustak/Utils.java
@@ -77,7 +77,7 @@ public class Utils {
                 Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
             }
         } catch (IOException e) {
-            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+            Log.e(LOG_TAG, "Problem retrieving the Books JSON results.", e);
         } finally {
             if (urlConnection != null) {
                 urlConnection.disconnect();
